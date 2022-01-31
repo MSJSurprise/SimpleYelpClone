d4567544bfe16150cae01a494e8d5ada8d0bd6f0
Surprise <mesanwoolasurprise303>
2022-01-31 02:07:38 -0800

    Updates

--- app/src/main/java/com/example/simpleyelpclone/MainActivity.kt
+++ app/src/main/java/com/example/simpleyelpclone/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.simpleyelpclone
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import retrofit2.Call
@@ -24,17 +25,23 @@ class MainActivity : AppCompatActivity() {
         rvRestaurant.adapter = adapter
         rvRestaurant.layoutManager = LinearLayoutManager(this)
 
+        val searchTerm = intent.getStringExtra("searchTerm")
+        val location = intent.getStringExtra("location")
+        Log.i(TAG, "searchTerm $searchTerm")
+        Log.i(TAG, "location $location")
 
         val retrofit = Retrofit.Builder().baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create()).build()
 
         val yelpService = retrofit.create(YelpService::class.java)
-        yelpService.searchRestaurants("Bearer $API_KEY","Avocado Toast", "New York").enqueue(object: Callback<YelpSearchResult>{
+        yelpService.searchRestaurants("Bearer $API_KEY","$searchTerm", "$location").enqueue(object: Callback<YelpSearchResult>{
             override fun onResponse(call: Call<YelpSearchResult>, response: Response<YelpSearchResult>) {
                 Log.i(TAG, "onResponse $response")
                 val body = response.body()
                 if (body == null) {
                     Log.w(TAG, "Did not receive valid response body from Yelp API... exiting")
+                    Toast.makeText(this@MainActivity, "Enter a valid Search Term and Location",
+                        Toast.LENGTH_LONG).show()
                     return
                 }
                 restaurants.addAll(body.restaurants)
--- app/src/main/java/com/example/simpleyelpclone/SearchTerm.kt
+++ app/src/main/java/com/example/simpleyelpclone/SearchTerm.kt
@@ -1,11 +1,35 @@
 package com.example.simpleyelpclone
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class SearchTerm : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search_term)
+
+        val edtSearchTerm: EditText = findViewById(R.id.edtSearchTerm)
+        val edtLocation: EditText = findViewById(R.id.edtLocation)
+        val btnSearch: Button = findViewById(R.id.btnSearch)
+
+        btnSearch.setOnClickListener {
+            val searchTerm = edtSearchTerm.text.toString().trim()
+            val location = edtLocation.text.toString().trim()
+            if (searchTerm.isEmpty() || location.isEmpty()) {
+                Toast.makeText(this, "Enter the search term and Location",
+                    Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            val intent = Intent(this, MainActivity::class.java)
+            intent.putExtra("searchTerm", searchTerm)
+            intent.putExtra("location", location)
+            startActivity(intent)
+        }
+
+
     }
 }
