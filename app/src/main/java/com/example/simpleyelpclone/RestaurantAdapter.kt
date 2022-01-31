c2c60e233764683c581ea702bab07c75bba7892d
Surprise <mesanwoolasurprise303>
2022-01-31 01:26:59 -0800

    Updates

--- app/src/main/java/com/example/simpleyelpclone/RestaurantAdapter.kt
+++ app/src/main/java/com/example/simpleyelpclone/RestaurantAdapter.kt
@@ -4,8 +4,16 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.RatingBar
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
+import org.w3c.dom.Text
+import java.util.*
 
 class RestaurantAdapter(val context: Context, val restaurants: List<YelpRestaurant>):RecyclerView.Adapter<RestaurantAdapter.ViewHolder>() {
 
@@ -26,7 +34,25 @@ class RestaurantAdapter(val context: Context, val restaurants: List<YelpRestaura
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         fun bind(restaurant: YelpRestaurant) {
             val tvName: TextView = itemView.findViewById(R.id.tvName)
+            val ivImage: ImageView = itemView.findViewById(R.id.imageView)
+            val ratingBar: RatingBar = itemView.findViewById(R.id.ratingBar)
+            val tvAddress: TextView = itemView.findViewById(R.id.tvAddress)
+            val tvCategory: TextView = itemView.findViewById(R.id.tvCategory)
+            val tvNumReviews: TextView = itemView.findViewById(R.id.tvNumReviews)
+            val tvDistance: TextView = itemView.findViewById(R.id.tvDistance)
+            val tvPrice: TextView = itemView.findViewById(R.id.tvPrice)
+
             tvName.text = restaurant.name
+           // ivImage.setImageResource(restaurant.imageUrl)
+            Glide.with(context).load(restaurant.imageUrl).apply(RequestOptions().transforms(
+                CenterCrop(), RoundedCorners(20)
+            )).into(ivImage)
+            ratingBar.rating = restaurant.rating.toFloat()
+            tvAddress.text = restaurant.location.address
+            tvCategory.text = restaurant.categories[0].title
+            tvNumReviews.text = "${restaurant.numReviews} Reviews"
+            tvDistance.text = restaurant.displayDistance()
+            tvPrice.text = restaurant.price
 
         }
     }
